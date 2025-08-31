fdc226769273fbe47e0b18cdf00e3d282dbf28e1
Henry Jonathan <henryjonathanchandra12@gmail.com>
2025-08-31 22:58:17 +0700

    try counter in Qwik js

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,4 +1,4 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useSignal } from "@builder.io/qwik";
 
 interface ProductProps {
     id: number;
@@ -8,14 +8,13 @@ interface ProductProps {
 }
 
 export const ProductCard = component$<ProductProps>(({ id, title, price, thumbnail }) => {
+    const count = useSignal(0);
     return (
         <div class="bg-gray-50 p-4 rounded-lg shadow-md hover:shadow-xl transition-shadow duration-300">
             <img src={thumbnail} alt={title} class="w-full h-48 object-cover mb-4 rounded" />
             <h2 class="text-lg font-semibold mb-2">{title}</h2>
             <p class="text-green-600 font-bold">${price}</p>
-            <button class="bg-green-600 hover:bg-green-700 text-white px-4 py-2 rounded-full mt-4">
-                Add to Cart
-            </button>
+            <button class="bg-green-600 hover:bg-green-700 text-white px-4 py-2 rounded-full mt-4" onClick$={() => count.value++}>Ditekan sebanyak: {count.value}</button>
         </div>
     );
 });
